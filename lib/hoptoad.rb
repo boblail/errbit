6933c48ae8724a7a6ade93d074637fb5768ffb12
Robert Lail <robert.lail@cph.org>
2011-06-01 10:01:22 -0500

    refactored hoptoad report parser

--- lib/hoptoad.rb
+++ lib/hoptoad.rb
@@ -1,6 +1,53 @@
+require 'digest/md5'
+
 module Hoptoad
   module V2
-    require 'digest/md5'
+    
+    
+    def self.parse_xml(xml)
+      parsed = ActiveSupport::XmlMini.backend.parse(xml)['notice']
+      raise ApiVersionError unless parsed && parsed['version'] == '2.0'
+      rekeyed = rekey(parsed)
+      rekeyed['fingerprint'] = get_fingerprint(rekeyed)
+      rekeyed
+    end
+    
+    
+  private
+    
+    
+    def self.rekey(node)
+      if node.is_a?(Hash) && node.has_key?('var') && node.has_key?('key')
+        {node['key'] => rekey(node['var'])}
+      elsif node.is_a?(Hash) && node.has_key?('var')
+        rekey(node['var'])
+      elsif node.is_a?(Hash) && node.has_key?('__content__') && node.has_key?('key')
+        {node['key'] => node['__content__']}
+      elsif node.is_a?(Hash) && node.has_key?('__content__')
+        node['__content__']
+      elsif node.is_a?(Hash)
+        node.inject({}) {|rekeyed, (key,val)|
+          rekeyed.merge(key => rekey(val))
+        }
+      elsif node.is_a?(Array) && node.first.has_key?('key')
+        node.inject({}) {|rekeyed, keypair|
+          rekeyed.merge(rekey(keypair))
+        }
+      elsif node.is_a?(Array)
+        node.map {|n| rekey(n)}
+      else
+        node
+      end
+    end
+    
+    
+    def self.get_fingerprint(notice)
+      Digest::MD5.hexdigest notice['error']['backtrace'].to_s
+    end
+    
+    
+  public
+    
     
     class ApiVersionError < StandardError
       def initialize
@@ -8,38 +55,6 @@ module Hoptoad
       end
     end
     
-    def self.parse_xml(xml)
-      parsed  = ActiveSupport::XmlMini.backend.parse(xml)['notice']
-      raise ApiVersionError unless parsed && parsed['version'] == '2.0'
-      rekeyed = rekey(parsed)
-      rekeyed['fingerprint'] = Digest::MD5.hexdigest(rekeyed['error']['backtrace'].to_s)
-      rekeyed
-    end
     
-    private
-      
-      def self.rekey(node)
-        if node.is_a?(Hash) && node.has_key?('var') && node.has_key?('key')
-          {node['key'] => rekey(node['var'])}
-        elsif node.is_a?(Hash) && node.has_key?('var')
-          rekey(node['var'])
-        elsif node.is_a?(Hash) && node.has_key?('__content__') && node.has_key?('key')
-          {node['key'] => node['__content__']}
-        elsif node.is_a?(Hash) && node.has_key?('__content__')
-          node['__content__']
-        elsif node.is_a?(Hash)
-          node.inject({}) {|rekeyed, (key,val)|
-            rekeyed.merge(key => rekey(val))
-          }
-        elsif node.is_a?(Array) && node.first.has_key?('key')
-          node.inject({}) {|rekeyed,keypair|
-            rekeyed.merge(rekey(keypair))
-          }
-        elsif node.is_a?(Array)
-          node.map {|n| rekey(n)}
-        else
-          node
-        end
-      end
   end
 end
